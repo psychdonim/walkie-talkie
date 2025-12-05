95ca68ee57bb417757754ca322f7780944d0938e
Ivshin Artemij <ivshin.artemij@yandex.ru>
2025-12-06 00:58:34 +0500

    done

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/psychdonim/walkie-talkie
+
+go 1.25.0
---
+++ pkg/walkietalkie.go
@@ -0,0 +1,107 @@
+package pkg
+
+import (
+	"encoding/binary"
+	"io"
+	"net"
+)
+
+const oneMessageBorder = 1 << 20 // 1 MB
+const chunkSize = 1<<16 - 1
+
+type WTCallbackChunk func([]byte)
+
+type WalkieTalkie struct {
+	listenAddr    net.Addr
+	chunksChannel chan []byte
+}
+
+func NewWalkieTalkie(listenAddr net.Addr) *WalkieTalkie {
+	return &WalkieTalkie{
+		listenAddr:    listenAddr,
+		chunksChannel: make(chan []byte, 4),
+	}
+}
+
+func (wt *WalkieTalkie) Listen() error {
+	listener, err := net.Listen(wt.listenAddr.Network(), wt.listenAddr.String())
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+		go wt.ListenFrames(conn)
+	}
+}
+
+func (wt *WalkieTalkie) ListenFrames(conn net.Conn) {
+	defer conn.Close()
+
+	header := make([]byte, 8)
+	if _, err := io.ReadFull(conn, header); err != nil {
+		return
+	}
+	messageLength := binary.BigEndian.Uint64(header)
+
+	chunk := make([]byte, chunkSize)
+	var offset uint64 = 0
+	for offset < messageLength {
+		n, err := conn.Read(chunk)
+		if err != nil {
+			return
+		}
+
+		wt.chunksChannel <- chunk[:n]
+		offset += uint64(n)
+	}
+}
+
+func (wt *WalkieTalkie) Send(messageReader io.Reader, messageLength uint64) error {
+	conn, err := net.Dial(wt.listenAddr.Network(), wt.listenAddr.String())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	if messageLength < oneMessageBorder {
+		oneshot := make([]byte, messageLength)
+		if _, err := messageReader.Read(oneshot); err != nil {
+			return err
+		}
+		_, err := conn.Write(oneshot)
+		return err
+	}
+
+	var offset uint64 = 0
+
+	startFrame := make([]byte, 8)
+	binary.BigEndian.PutUint64(startFrame, messageLength)
+	if _, err := conn.Write(startFrame); err != nil {
+		return err
+	}
+
+	chunk := make([]byte, chunkSize)
+	for offset < messageLength {
+		n, err := messageReader.Read(chunk)
+		if err != nil {
+			return err
+		}
+
+		frameLen := 2 + n
+		frame := make([]byte, frameLen)
+		binary.BigEndian.PutUint16(frame[:2], uint16(n))
+		copy(frame[2:], chunk)
+
+		if _, err := conn.Write(frame); err != nil {
+			return err
+		}
+		offset += uint64(n)
+	}
+
+	return nil
+}
